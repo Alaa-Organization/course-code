7854ac3d5a107a7d14439150caf3d74b73770754
Keyhan Saldjoughi <keyhan.saldjoughi@gmail.com>
2026-01-21 15:18:15 +0100

    Compiled JS

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -3,6 +3,14 @@ https://github.com/actions/toolkit
 npm i @actions/core @actions/github
 --------------------------------------------------------*/
 const core = require("@actions/core");
+/* core does not exist inside the runner & we don't want to push node_modules
+  so we have to compile the requirements in a file
+  using https://github.com/vercel/ncc
+  with npm i -g @vercel/ncc
+  then ncc build input.js -o dist
+  .. navigate to hello in terminal
+  .. ncc build index.js
+*/
 const github = require("@actions/github");
 
 try {
