3a124faa4509da5bdd861acbd76b4f517fa2bcba
Keyhan Saldjoughi <keyhan.saldjoughi@gmail.com>
2026-01-21 17:03:18 +0100

    Docker Custom Action

--- .github/actions/hello-docker/entrypoint.sh
+++ .github/actions/hello-docker/entrypoint.sh
@@ -1,14 +1,14 @@
+#!/bin/sh
 #-----------------------------------------------------
 # just like index.js was compiled, this files needs to
 # be an executable file with navigating to the hello-docker
 # folder with open windows terminal:
 # git update-index --add --chmod=+x entrypoint.sh
 #------------------------------------------------------
-#!/bin/sh
 echo "::debug::Running entrypoint.sh"
 echo "Hello $@"
 echo "INPUT_WHO_TO_GREET: $INPUT_WHO_TO_GREET"
 echo "HELLO: $HELLO"
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
-echo "HELLO_TIME" >> $GITHUB_ENV
+echo "HELLO_TIME=$time" >> $GITHUB_ENV
